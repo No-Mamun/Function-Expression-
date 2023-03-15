0b7bb028900b8a4897c66be3b6c7507143ee41b2
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-03-15 15:38:18 +0600

    Add files via upload
    Function Expression,

---
+++ main.js
@@ -0,0 +1,120 @@
+// Function Expression, Named Function etc
+
+/*
+// No:1
+
+function display(){
+
+              console.log("Hello World");
+
+}
+
+display();
+
+// No:2
+
+function display(Massage){
+
+              console.log(Massage);
+
+}
+
+display("Hello World");
+
+No:2
+
+function mamun(){
+
+              console.log("mamun khan2");
+
+}
+
+mamun();
+
+No:3
+
+var display2 = function mamun(){
+
+              console.log("Hi Mamun Khan");
+
+}
+display2();
+
+
+No:4
+
+const checkage = function(age){
+
+if(age<18){
+
+console.log("you are too old");
+
+}else{
+
+              console.log("you are too eligible");
+
+}
+
+}
+checkage(30);
+
+No:5
+
+// named function Expression
+
+const doMath = {
+
+something:function(age){
+
+if(age <18){
+
+console.log("you are too old");
+
+}else{
+
+console.log("you are too eligible");
+
+}
+
+
+},
+
+add:function (a,b){
+
+return a + b;
+
+}
+
+};
+
+doMath.something(30);
+
+console.log(doMath);
+
+console.log(doMath.add(53,3));
+
+No:6
+
+// named function Expression
+
+let age = prompt ("How old are you");
+
+if(age <18){
+
+function welcom(){
+
+alert("Hi How are you")
+
+}
+
+} else{
+              function welcom(){
+
+alert("what!s up?")
+
+}
+
+}
+welcom();
+
+*/
